bd7a8953dd5a533a6a50d4173570e0cf64f8132f
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2021-04-15 22:36:54 +0530

    Add files via upload

---
+++ precache-manifest.f2a6278238643c42560738d3c26d51c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b020004b4adc729d74b",
+    "url": "/image-gallery/static/css/main.2469456e.chunk.css"
+  },
+  {
+    "revision": "1b020004b4adc729d74b",
+    "url": "/image-gallery/static/js/main.1b020004.chunk.js"
+  },
+  {
+    "revision": "5a394a403732528c61c5",
+    "url": "/image-gallery/static/js/1.5a394a40.chunk.js"
+  },
+  {
+    "revision": "d2ee350bdb6e4042e442",
+    "url": "/image-gallery/static/js/runtime~main.d2ee350b.js"
+  },
+  {
+    "revision": "b371e2605354783f2dd9139307c39134",
+    "url": "/image-gallery/index.html"
+  }
+];
